067e74128ef85627e6ff0cd3469828e9d6446a5b
Matheus Serpa <mserpa@flille>
2019-07-19 01:48:12 +0200

    fixing again

--- exp/DoE.sh
+++ exp/DoE.sh
@@ -3,7 +3,7 @@
 set -o errexit -o nounset -o pipefail -o posix
 
 declare -a MAPPING=("os" "homo" "hetero")
-declare -a APP=("ctl-complex" "ctl-conditional" "ctl-random" "ctl-small-bbl" "ctl-switch" "dep-chain1" "dep-chain2" "dep-chain3" "dep-chain4" "dep-chain5" "dep-chain6" "fp-add" "fp-div" "fp-mul" "int-add" "int-div" "int-mul" "load-dep:32" "load-ind:32" "load-rand:32" "idle"),
+declare -a APP=("ctl-complex" "ctl-conditional" "ctl-random" "ctl-small-bbl" "ctl-switch" "dep-chain1" "dep-chain2" "dep-chain3" "dep-chain4" "dep-chain5" "dep-chain6" "fp-add" "fp-div" "fp-mul" "int-add" "int-div" "int-mul" "load-dep:32" "load-ind:32" "load-rand:32" "idle")
 
 OUTPUT=./DoE/`hostname`.csv
 rm -f $OUTPUT
