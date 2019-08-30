6251fe8582f061227576e082987b8baf571d2eaa
msserpa <matheusserpa@gmail.com>
2019-08-30 15:02:52 -0300

    changing doe

--- exp/DoE.sh
+++ exp/DoE.sh
@@ -3,7 +3,8 @@
 #set -o errexit -o nounset -o pipefail -o posix
 
 declare -a MAPPING=("os" "homo" "hetero")
-declare -a APP=("ctl-complex" "dep-chain1" "fp-add" "fp-div" "fp-mul" "int-add" "int-div" "int-mul" "load-dep:16" "idle")
+declare -a APP=("ctl-complex" "dep-chain1" "fp-add" "fp-div" "fp-mul" "int-add" "int-div" "int-mul" "load-dep:32" "load-ind:32" "load-rand:32" "load-dep:64" "load-ind:64" "load-rand:64" "load-dep:128" "load-ind:128" "load-rand:128" "load-dep:512" "load-ind:512" "load-rand:512" "load-dep:1024" "load-ind:1024" "load-rand:1024" "load-dep:2048" "load-ind:2048" "load-rand:2048" "load-dep:4096" "load-ind:4096" "load-rand:4096" "idle")
+#declare -a APP=("ctl-complex" "dep-chain1" "fp-add" "fp-div" "fp-mul" "int-add" "int-div" "int-mul" "load-dep:16" "idle")
 #declare -a APP=("ctl-complex" "ctl-conditional" "ctl-random" "ctl-small-bbl" "ctl-switch" "dep-chain1" "dep-chain2" "dep-chain3" "dep-chain4" "dep-chain5" "dep-chain6" "fp-add" "fp-div" "fp-mul" "int-add" "int-div" "int-mul" "load-dep:16" "load-ind:16" "load-rand:16" "load-dep:8192" "load-ind:8192" "load-rand:8192" "idle")
 
 OUTPUT=./DoE/`hostname`.csv
